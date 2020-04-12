672f76eec544b4ba67ba41deee7c3bcfaf96eac9
lisacampbell <59990057+lisaswindler@users.noreply.github.com>
2020-04-11 22:35:38 -0600

    finished project

--- train.js
+++ train.js
@@ -1,12 +1,12 @@
 var firebaseConfig = {
-apiKey: "AIzaSyBTAcmimR_OROyeKgCbuouoXm_gxVUJtNc",
-authDomain: "lisa-project-7e982.firebaseapp.com",
-databaseURL: "https://lisa-project-7e982.firebaseio.com",
-projectId: "lisa-project-7e982",
-storageBucket: "lisa-project-7e982.appspot.com",
-messagingSenderId: "1085149494361",
-appId: "1:1085149494361:web:8af8e9bc545c57dc24c548"
-};
+    apiKey: "AIzaSyBTAcmimR_OROyeKgCbuouoXm_gxVUJtNc",
+    authDomain: "lisa-project-7e982.firebaseapp.com",
+    databaseURL: "https://lisa-project-7e982.firebaseio.com",
+    projectId: "lisa-project-7e982",
+    storageBucket: "lisa-project-7e982.appspot.com",
+    messagingSenderId: "1085149494361",
+    appId: "1:1085149494361:web:dabd8a7890b61c7e24c548"
+  };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -14,54 +14,62 @@ firebase.initializeApp(firebaseConfig);
 // Assign the reference to the database to a variable named 'database'
 var database = firebase.database();
 
-var trainDest = trainDest;
-var trainFreq = trainFreq;
+var trainDest;
+var trainFreq;
+var minutesAway;
+var firstTrain;
+var arrivalTime;
 
-database.ref().on("value", function(snapshot) {
+database.ref("/trains").on("child_added", function(childSnapshot) {
+// database.ref("/trains").on("value", function(childSnapshot) {
+        snap = childSnapshot.val();
+    // If firebase has a train stored
+    if (childSnapshot.child("trainDest").exists()) {
 
-    // If Firebase has a train stored
-    if (snapshot.child("trainDest").exists()) {
+        // Get the values from firebase
+        trainDest = snap.trainDest;
+        trainFreq = snap.trainFreq;
+        firstTrain = snap.firstTrain;
+        arrivalTime = snap.arrivalTime;
+        minutesAway = snap.minutesAway;
 
-        trainDest = snapshot.val().trainDest;
-        trainFreq = snapshot.val().trainFreq;
-
+        // Moment.js conversions
+        var diffTimes = moment.duration(moment().diff(moment(firstTrain, "HH:mm")), 'milliseconds').asMinutes();
+        var minutesAway = snap.trainFreq - (Math.floor(diffTimes) % trainFreq);
+        var arrivalTime = moment().add(minutesAway, 'minutes').format('HH:mm');
+     
         // Change the HTML to reflect the stored values
-        $('#train-table').append('<tr><td>' + trainDest + '</td><td>' + trainFreq + '</td></tr>');
-
-    };
+        $('#train-table').append(
+        '<tr><td>' + trainDest + 
+        '</td><td>' + trainFreq + 
+        '</td><td>' + arrivalTime + 
+        '</td><td>' + minutesAway + 
+        '</td></tr>'
+        );
+    }
+    }, function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
 });
 
 $("#submit").on("click", function(event) {
     // Prevent form from submitting
     event.preventDefault();
     
-// Get the input values
+    // Get the input values
     var trainDest = $("#train-destination").val().trim();
     var trainFreq = Number($("#train-frequency").val().trim());
     var firstTrain = $("#first-train").val().trim();
-
-// Moment.js conversions
-    var firstTrainConverted = moment(firstTrain).format("h:mm:ss");
-    console.log("Converted Time: " + firstTrainConverted);
-
-    var diffTime = moment.duration(moment().diff(moment(firstTrain, "HH:mm")), 'milliseconds').asMinutes();
-    console.log("DIFFERENCE IN TIME: " + diffTime);
-  
-    var minutesAway = trainFreq - (Math.floor(diffTime) % trainFreq);
-    console.log(minutesAway);
-  
-    var nextArrival = diffTime > 0 ? moment().add(minutesAway, 'minutes' ) : moment(firstTrain, "HH:mm") ;
-    console.log("ARRIVAL TIME: " + moment(nextArrival).format("HH:mm"));
-    
-    var minTilTrain = Math.ceil(moment.duration(moment(nextArrival).diff(moment()), 'milliseconds').asMinutes());
-    console.log("MINUTES TILL TRAIN: " + minTilTrain);
-
+    var diffTimes = moment.duration(moment().diff(moment(firstTrain, "HH:mm")), 'milliseconds').asMinutes();
+    var minutesAway = trainFreq - (Math.floor(diffTimes) % trainFreq);
+    var arrivalTime = moment().add(minutesAway, 'minutes').format('HH:mm');
+        
     // Save the new train in Firebase
-    database.ref().set({
+    database.ref().child('/trains').push ({
     trainDest: trainDest,
     trainFreq: trainFreq,
-    firstTrain: firstTrain
+    firstTrain: firstTrain,
+    arrivalTime: arrivalTime,
+    minutesAway: minutesAway
     });
 
-    $('#train-table').append('<tr><td>' + trainDest + '</td><td>' + trainFreq + '</td></tr>');
 });
